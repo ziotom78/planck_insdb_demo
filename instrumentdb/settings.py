1ff998c2a386ce7dd4b0d1cafbde70b355b7cb7f
Maurizio Tomasi <ziotom78@gmail.com>
2022-02-14 13:24:35 +0100

    Check that the token is not expiredd, setup admin pages

--- instrumentdb/settings.py
+++ instrumentdb/settings.py
@@ -143,8 +143,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 LOGIN_REDIRECT_URL = "/accounts/login/"
 LOGOUT_REDIRECT_URL = "/"
+ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 TOKEN_EXPIRED_AFTER_MINUTES = 15
 
