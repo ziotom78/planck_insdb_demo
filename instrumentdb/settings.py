9973c4eff4c436412e9c840271cd7a767c063755
Maurizio Tomasi <ziotom78@gmail.com>
2022-11-24 19:59:26 +0100

    Reformat the code using black

--- instrumentdb/settings.py
+++ instrumentdb/settings.py
@@ -21,7 +21,7 @@ from envparse import Env
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-DEFAULT_AUTO_FIELD='django.db.models.AutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 env = Env(
     DEBUG=bool,
@@ -144,10 +144,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 LOGIN_REDIRECT_URL = "/accounts/login/"
 LOGOUT_REDIRECT_URL = "/"
-ADMIN_MEDIA_PREFIX = '/static/admin/'
+ADMIN_MEDIA_PREFIX = "/static/admin/"
 
 TOKEN_EXPIRED_AFTER_MINUTES = 15
 
